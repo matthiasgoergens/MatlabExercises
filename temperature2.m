737dc57217ce19e334103af4d7da117949df83dc
Matthias Görgens <matthias.goergens@gmail.com>
2017-04-02 11:24:50 +0100

    Minimum temperature is 5 degrees

--- temperature2.m
+++ temperature2.m
@@ -1,8 +1,5 @@
 function T=temperature2(t)
     beta=15/3600;
     T0=56.2; %intial temperature
-    T=T0-beta*(t);
-    if T<5;
-        T=5;
-    end
+    T=max(5, T0-beta*(t));
 end
--- temperature3.m
+++ temperature3.m
@@ -1,5 +1,5 @@
 function T=temperature3(findbeta)
 time=(0:1/60:10);
 T0=56.2; %intial temperature
-T=T0-findbeta*(time);
+T=max(5, T0-findbeta*time);
 end
